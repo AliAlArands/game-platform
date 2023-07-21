734fc9c972eb4a82b3358cd4f27f756e8378b92e
ali darwish <alidarwish502@gmail.com>
2023-07-21 07:44:46 +0300

    Refactored the Games to remove duplicated code.

--- src/components/Games.tsx
+++ src/components/Games.tsx
@@ -28,29 +28,7 @@ const Games = () => {
   const { error, data: games, isLoading } = useGames();
 
   const skeletons = [1, 2, 3, 4, 5, 6];
-  if (isLoading)
-    return (
-      <Grid
-        templateColumns={{
-          lg: "repeat(3, 1fr)",
-          md: "repeat(2, 1fr)",
-          base: "1fr",
-        }}
-        gap="1rem"
-        rowGap="2rem"
-      >
-        {skeletons.map((skeleton) => (
-          <Card width={"350px"}>
-            <CardHeader>
-              <Skeleton height="350px" borderRadius={"10px"} key={skeleton} />
-            </CardHeader>
-            <CardBody>
-              <SkeletonText />
-            </CardBody>
-          </Card>
-        ))}
-      </Grid>
-    );
+
   return (
     <>
       {error && <Text color="red.200">{error.message}</Text>}
@@ -62,6 +40,17 @@ const Games = () => {
         }}
         gap="1rem"
       >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <Card width={"300px"} borderRadius={"10px"}>
+              <CardHeader>
+                <Skeleton height="250px" borderRadius={"10px"} key={skeleton} />
+              </CardHeader>
+              <CardBody>
+                <SkeletonText />
+              </CardBody>
+            </Card>
+          ))}
         {games?.map((game) => (
           <GridItem key={game.id}>
             <GameCard game={game} />
